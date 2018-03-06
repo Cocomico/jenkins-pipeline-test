80422bc89032552e4912423290d4aa473e716d97
Sandra Rapp <SandraRapp@de.ibm.com>
2018-03-06 11:31:18 +0100

    modify script

--- myscript.sh
+++ myscript.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 printf "Running myscript.sh\n"
-printf "Using branch ${branchName}"
-pwd
-ls -al
+printf "Using branch ${branchName}\n"
+echo "which node"
+which node
+echo "npm install"
 npm install
+echo "ls -al node_modules"
+ls -al node_modules
